d9a18bac8b3e0fe18154d715b2a5fb8b6f198518
Przemyslaw Jankowski <przemyslaw.lukasz.jankowski@gmail.com>
2022-01-27 00:28:54 +0100

    7.2. Non-mutable forum user

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,9 +2,11 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
 import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.lambda.Processor;
 import com.kodilla.stream.reference.FunctionalCalculator;
+import java.util.stream.*;
 
 import java.util.Locale;
 
@@ -37,5 +39,8 @@ public class StreamMain {
         poemBeautifier.beautify(textToDecor, (text) -> text.toUpperCase());
         poemBeautifier.beautify(textToDecor, (text) -> text.toLowerCase());
         poemBeautifier.beautify(textToDecor, (text) -> text.repeat(5));
+
+        System.out.println("\nUsing Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
