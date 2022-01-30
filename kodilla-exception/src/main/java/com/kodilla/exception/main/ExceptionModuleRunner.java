6f527a827bcabae8de33fee1bf0cbb5386275d0a
Przemyslaw Jankowski <przemyslaw.lukasz.jankowski@gmail.com>
2022-01-30 19:29:09 +0100

    8.2. try! catch! finally!

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.main;
+
+import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+
+public class ExceptionModuleRunner {
+
+//    public static void main(String[] args) {
+//        FileReader fileReader = new FileReader();
+//        fileReader.readFile();
+//
+//        FileReaderWithoutHandling fileReader2 = new FileReaderWithoutHandling();
+//        fileReader2.readFile();
+//    }
+}
